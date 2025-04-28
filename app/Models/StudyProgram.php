5ccb96fb80ae57172df5060bc873b37ba2ca8ae6
dmitru4ok <yurii.dmytruk@mif.stud.vu.lt>
2025-04-28 16:18:10 +0300

    adjust StudyProgram and StudyProgramInstance models according to DB model

--- app/Models/StudyProgram.php
+++ app/Models/StudyProgram.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class StudyProgram extends Model
 {
-    //
+    protected $primaryKey = null;
+    public $incrementing = false;
+    public $timestamps = false;
+    
+    protected $fillable = [
+        'program_code',
+        'program_name',
+        'program_type',
+        'faculty_code'
+    ];
+
+    function instances() {
+        return $this->hasMany(StudyProgramInstance::class, "program_code", "program_code");
+    }
 }
