74e7c001afc45516b25ad3b78a357e1c64e4344c
dmitru4ok <yurii.dmytruk@mif.stud.vu.lt>
2025-05-11 18:36:08 +0300

    test registration /register endpoint

--- tests/Feature/AuthEndpointsTest.php
+++ tests/Feature/AuthEndpointsTest.php
@@ -11,7 +11,7 @@ use function PHPUnit\Framework\assertEquals;
 class AuthEndpointsTest extends TestCase
 {
     use RefreshDatabase;
-    
+
     public function test_whoamiUnathenticated_fail() {
         $response = $this->getJson('/me');
 
@@ -45,5 +45,151 @@ class AuthEndpointsTest extends TestCase
         $response->assertJsonIsObject();
         assertEquals(auth('api')->user()->role, 'S');
         $this->assertDatabaseHas('users', ['user_id' => auth('api')->user()->user_id, 'role' => auth('api')->user()->role]);
+    }
+
+    public function test_registerUnathenticated_fail() {
+        $response = $this->postJson('/register');
+
+        $response->assertStatus(401);
+        $response->assertJsonIsObject();
+        $response->assertExactJson(['message' => 'Unauthenticated.']);
+    }
+
+    public function test_registerAuthenticatedAsStud_fail() {
+        $response = $this->asUserRole('S')->postJson('/register');
+
+        $response->assertStatus(403);
+        $response->assertJsonIsObject();
+        $response->assertExactJson(['message' => 'No privilege to access this resource']);
+    }
+
+    public function test_registerAuthenticatedAsProf_fail() {
+        $response = $this->asUserRole('P')->postJson('/register');
+
+        $response->assertStatus(403);
+        $response->assertJsonIsObject();
+        $response->assertExactJson(['message' => 'No privilege to access this resource']);
+    }
+
+    public function test_registerAuthenticatedAsAdmin_Create_S_RigthParams_success() {
+        $study_pr_year_to_insert_with = 2023;
+        $study_pr_code_to_insert_with = 'BX0012';
+        $this->assertDatabaseHas('study_program_instances', [
+            'program_code' => $study_pr_code_to_insert_with,
+            'year_started' => $study_pr_year_to_insert_with
+        ]);
+
+        $this->assertDatabaseMissing('users', ['email' => 'example@example.com', 'role' => 'S']);
+        $response = $this->asUserRole('A')->postJson('/register', [
+            'name' => 'John',
+            'surname' => 'Doe',
+            'email' => 'example@example.com',
+            'role' => 'S',
+            'year_started' => $study_pr_year_to_insert_with,
+            'program_code' => $study_pr_code_to_insert_with,
+            'password' => 'blabla1234'
+        ]);
+
+        $response->assertStatus(201);
+        $response->assertJsonIsObject();
+        $response->assertJson([
+            'name' => 'John',
+            'surname' => 'Doe',
+            'email' => 'example@example.com',
+            'role' => 'S',
+            'program_code' => $study_pr_code_to_insert_with,
+            'year_started' => $study_pr_year_to_insert_with
+        ]);
+
+        $this->assertDatabaseHas('users', [
+            'name' => 'John',
+            'surname' => 'Doe',
+            'email' => 'example@example.com',
+            'role' => "S",
+            'program_code' => $study_pr_code_to_insert_with,
+            'year_started' => $study_pr_year_to_insert_with
+        ]);
+    }
+
+    public function test_registerAuthenticatedAsAdmin_Create_A_RigthParams_success() {
+        $study_pr_year_to_insert_with = 2023;
+        $study_pr_code_to_insert_with = 'BX0012';
+        $this->assertDatabaseHas('study_program_instances', [
+            'program_code' => $study_pr_code_to_insert_with,
+            'year_started' => $study_pr_year_to_insert_with
+        ]);
+
+        $this->assertDatabaseMissing('users', ['email' => 'example@example.com', 'role' => 'A']);
+        $response = $this->asUserRole('A')->postJson('/register', [
+            'name' => 'John',
+            'surname' => 'Doe',
+            'email' => 'example@example.com',
+            'role' => 'A',
+            'year_started' => $study_pr_year_to_insert_with,
+            'program_code' => $study_pr_code_to_insert_with,
+            'password' => 'blabla1234'
+        ]);
+
+        $response->assertStatus(201);
+        $response->assertJsonIsObject();
+        $response->assertJson([
+            'name' => 'John',
+            'surname' => 'Doe',
+            'email' => 'example@example.com',
+            'role' => 'A'
+        ]);
+
+        $response->assertJsonPath('program_code', null);
+        $response->assertJsonPath('year_started', null);
+
+        $this->assertDatabaseHas('users', [
+            'name' => 'John',
+            'surname' => 'Doe',
+            'email' => 'example@example.com',
+            'role' => "A",
+            'program_code' => null,
+            'year_started' => null
+        ]);
+    }
+
+    public function test_registerAuthenticatedAsAdmin_Create_P_RigthParams_success() {
+        $study_pr_year_to_insert_with = 2023;
+        $study_pr_code_to_insert_with = 'BX0012';
+        $this->assertDatabaseHas('study_program_instances', [
+            'program_code' => $study_pr_code_to_insert_with,
+            'year_started' => $study_pr_year_to_insert_with
+        ]);
+
+        $this->assertDatabaseMissing('users', ['email' => 'example@example.com', 'role' => 'P']);
+        $response = $this->asUserRole('A')->postJson('/register', [
+            'name' => 'John',
+            'surname' => 'Doe',
+            'email' => 'example@example.com',
+            'role' => 'P',
+            'year_started' => $study_pr_year_to_insert_with,
+            'program_code' => $study_pr_code_to_insert_with,
+            'password' => 'blabla1234'
+        ]);
+
+        $response->assertStatus(201);
+        $response->assertJsonIsObject();
+        $response->assertJson([
+            'name' => 'John',
+            'surname' => 'Doe',
+            'email' => 'example@example.com',
+            'role' => 'P'
+        ]);
+
+        $response->assertJsonPath('program_code', null);
+        $response->assertJsonPath('year_started', null);
+
+        $this->assertDatabaseHas('users', [
+            'name' => 'John',
+            'surname' => 'Doe',
+            'email' => 'example@example.com',
+            'role' => "P",
+            'program_code' => null,
+            'year_started' => null
+        ]);
     }
 }
