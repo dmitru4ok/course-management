9e090b65b4e3f5249881f6e9a7af3dd7b107eb8d
dmitru4ok <yurii.dmytruk@mif.stud.vu.lt>
2025-05-12 14:41:40 +0300

    Some study program controller functionality tests

--- tests/Feature/StudyProgramTest.php
+++ tests/Feature/StudyProgramTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\StudyProgramInstance;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -26,37 +27,64 @@ class StudyProgramTest extends TestCase
         ]);
     }
 
-    public function test_getStudyProgramsAsProf_noArgs_allStudyPrograms()
+    public function test_getStudyPrograminstancesAsAdmin_noArgs_allStudyProgramInstances()
     {
-        $response = $this->asUserRole('P')->getJson('/study_programs');
+        $response = $this->asUserRole('A')->getJson('/study_programs/instances');
 
         $response->assertStatus(200);
         $response->assertJsonIsArray();
         $response->assertExactJsonStructure([
             '*' => [
                 'program_code',
-                'program_name',
-                'faculty_code',
-                'program_type',
-                'is_valid'
+                'year_started',
+                'is_active'
             ]
         ]);
     }
 
-    public function test_getStudyProgramsAsStudnoArgs_allStudyPrograms()
+    public function test_getStudyProgramByCodeAsAdmin_OneStudyProgram()
     {
-        $response = $this->asUserRole('S')->getJson('/study_programs');
+        $program_code = 'BX0012';
+        $response = $this->asUserRole('S')->getJson('/study_programs/'. $program_code);
+        $this->assertDatabaseHas('study_programs', [
+            'program_code' => $program_code, 
+            'program_type' => 'B', 
+            'is_valid'=>true
+        ]);
 
         $response->assertStatus(200);
+        $response->assertJsonIsObject();
+        $response->assertExactJsonStructure([
+            'program_code',
+            'program_name',
+            'is_valid',
+            'program_type',
+            'faculty_code'
+        ]);
+    }
+    
+    public function test_getStudyProgramInstancesByCodeAsAdmin_OneStudyProgram()
+    {
+        $program_code = 'BX0012';
+        $response = $this->asUserRole('A')->getJson('/study_programs/'. $program_code.'/instances');
+        $this->assertDatabaseHas('study_programs', [
+            'program_code' => $program_code, 
+            'program_type' => 'B', 
+            'is_valid'=>true
+        ]);
+
+        $response->assertStatus(200);
+
         $response->assertJsonIsArray();
+
         $response->assertExactJsonStructure([
             '*' => [
                 'program_code',
-                'program_name',
-                'faculty_code',
-                'program_type',
-                'is_valid'
+                'year_started',
+                'is_active'
             ]
         ]);
+        $response->assertJsonCount(StudyProgramInstance::where('program_code', $program_code)->count());
     }
+    
 }
