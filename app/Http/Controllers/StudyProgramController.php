3968968c727b307e39fe5dec8a578e7086273add
dmitru4ok <yurii.dmytruk@mif.stud.vu.lt>
2025-05-25 05:55:36 -0700

    backend for deep-copying study prog instance's semesters and compulsory courses

--- app/Http/Controllers/StudyProgramController.php
+++ app/Http/Controllers/StudyProgramController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StudyProgramRequest;
 use App\Models\Semester;
+use App\Models\SemesterCourseRequirement;
 use App\Models\StudyProgramInstance;
 use App\Models\StudyProgram;
 use DateTime;
@@ -72,27 +73,43 @@ class StudyProgramController extends Controller
         $data = $request->validated();
         $new_instance = StudyProgramInstance::create($data);
         if (!is_null($data['perform_deep_copy']) && $data['perform_deep_copy']) {
-            $old_semesters  = StudyProgramInstance::where('program_code', $data['copy_program_code'])
-                ->where('year_started', $data['copy_program_year'])->first()->semesters;
-                $new_semesters = $old_semesters->map(function ($semester) use ($data) {
-                    return [
+            $copy_code = $data['copy_program_code'];
+            $copy_year = $data['copy_program_year'];
+            $old_semesters = StudyProgramInstance::where('program_code', $copy_code)
+                ->where('year_started', $copy_year)->first()->semesters;
+                $new_semesters = [];
+                $new_compulsories = [];
+                $yr_diff = $data['year_started'] - $copy_year;
+                foreach ($old_semesters as $old_semester) {
+                    $new_sem = [
                         'program_code' => $data['program_code'],
                         'year_started' => $data['year_started'],
-                        'sem_no' => $semester->sem_no,
-                        'is_valid' => $semester->is_valid,
-                        'date_from' => new DateTime($semester->date_from)->modify('+1 year')->format('Y-m-d'),
-                        'date_to' =>  new DateTime($semester->date_to)->modify('+1 year')->format('Y-m-d'),
+                        'sem_no' => $old_semester->sem_no,
+                        'is_valid' => $old_semester->is_valid,
+                        'date_from' => new DateTime($old_semester->date_from)->modify("+$yr_diff year")->format('Y-m-d'),
+                        'date_to' =>  new DateTime($old_semester->date_to)->modify("+$yr_diff year")->format('Y-m-d'),
                     ];
-                })->toArray();
-                dd($old_semesters->toArray(), Semester::insert($new_semesters));
-            // TODO
-            // for each semester get compulsory courses
-            // instert semesters and their courses for the new instance
+                    $new_semesters[] = $new_sem;
 
-            // use Model::insert() for bulk insert
+                    $sem_comp_courses = SemesterCourseRequirement::where([
+                        ['program_code', '=', $old_semester->program_code],
+                        ['year_started', '=', $old_semester->year_started],
+                        ['sem_no', '=', $old_semester->sem_no]
+                    ])->get('course_code')->map(function(SemesterCourseRequirement $course_code_to_duplicate) use ($new_sem) {
+                        return [
+                            'program_code' => $new_sem['program_code'],
+                            'year_started' => $new_sem['year_started'],
+                            'sem_no' => $new_sem['sem_no'],
+                            'course_code' => $course_code_to_duplicate->course_code
+                        ];
+                    });
+                    array_push($new_compulsories, ...$sem_comp_courses);
+                }
+                Semester::insert($new_semesters);
+                SemesterCourseRequirement::insert($new_compulsories);
         }
        
-        // return $new_instance;
+        return $new_instance;
     }
 
     public function update(Request $request, string $code)
